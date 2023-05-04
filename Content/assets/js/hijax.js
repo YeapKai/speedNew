cadc811e179d0d74ec578d7971f79592c2ffad60
YeapKai <yeaphk-pm21@student.tarc.edu.my>
2023-05-04 14:38:17 +0800

    Added new files

---
+++ Content/assets/js/hijax.js
@@ -0,0 +1,13 @@
+function loadheader()
+{ 
+   $("#includedheader").load("header.html");
+}
+function loadfooter()
+{
+   $("#includedfooter").load("footer.html");
+}
+function loadcopyright()
+{
+   $("#includedcopyright").load("footer-copyright.html");
+}
+
